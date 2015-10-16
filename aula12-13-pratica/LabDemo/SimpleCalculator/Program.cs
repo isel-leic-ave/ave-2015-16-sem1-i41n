d5d4c10e66f5c0bf34a42241e9672a1550190e2f
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-10-16 11:26:55 +0100

    Visual Studio project example with unit test

---
+++ aula12-13-pratica/LabDemo/SimpleCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    public class Operations
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ aula12-13-pratica/LabDemo/SimpleCalculatorTest/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SimpleCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculatorTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            Assert.AreEqual(2, Operations.Add(1, 1));
+        }
+
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            Assert.AreEqual(1, Operations.Sub(2, 1));
+        }
+    }
+}
