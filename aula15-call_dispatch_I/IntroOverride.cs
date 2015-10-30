6bb8ed104d486c2d88824e3167de563e142a3b41
Jose Simao <jsimao@cc.isel.ipl.pt>
2015-10-30 12:29:26 +0000

    Aula 15: Virtual and non-virtual instance methods

---
+++ aula15-call_dispatch_I/IntroOverride.cs
@@ -0,0 +1,23 @@
+
+
+using System;
+
+class A {
+	public virtual void M() { Console.WriteLine("A.M()"); }
+	public void T()         { Console.WriteLine("A.T()"); }
+}
+
+class C : A {
+	int field=1;
+	public override void M() { Console.WriteLine("C.M()"); }
+}
+
+class Program {
+
+
+	static void Main() {
+		A a = new C();  
+		a.M();
+		((A)a).M();
+	}
+}
